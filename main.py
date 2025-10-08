90be3e99071b9777ce9058a8ae3da7a74da1d5a1
adichoudhary02 <adityachoudhary0221@gmail.com>
2025-10-08 15:05:24 +0530

    Covered the further concepts of the python

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ my_test = None
 
 #To accept input from the user - inpur("Text to be displayed ")
 name = None
+
+
+print("="*20)
+
 # name = input("Enter your name: ")
 if name is None:
     name = "Adi"
@@ -46,6 +50,8 @@ type(<variable>) funcation which return the type of the datatype
 type(<variable>).__name__ funcation which returns the exect name of data type
 """
 
+print("="*20)
+
 def area_of_circle(radius):
     pi = 3.14
     result = pi * radius * radius
@@ -55,3 +61,78 @@ radius = 5
 area = area_of_circle(radius)
 print(f"Area of the circle is: {area}")
 
+"""
+Okey how the execution take place is given here it moves in the chronological order
+1. The fucation is defined for the later use and it has for in input variable radius body is skipped for now.
+2. Radius variable is declared and intialized with values 5
+3. Now the function variable is called with input value as the radius
+4. then redius is assigned the radius variable value. 
+5. body is executed returned the result variable value.
+6. area is assigned the value of the result of the funcation
+7. print the line and the value of the area
+"""
+
+#All of the fucation need to be defined before they are called:
+#For this use the main funcation approch
+
+print("=")
+
+def main():
+    health = 10
+    armor = 5
+    add_armor(health, armor)
+
+def add_armor(h, a):
+    new_health = h + a
+    print_health(new_health)
+
+def print_health(new_health):
+    print(f"The player now has {new_health} health")
+
+# call entrypoint last
+main()
+
+"""
+The main fuction deff:
+def main():
+
+#Define all fxn here
+
+#Define all fxn here
+
+#Entery Point
+main()
+"""
+
+#All of these 3 functions return the none as the output
+def my_func():
+    print("I do nothing")
+    return None
+
+def my_func2():
+    print("I do nothing")
+    return
+
+def my_func3():
+    print("I do nothing")
+
+
+#We can return the multiple values in python
+
+def cast_iceblast(wizard_level, start_mana):
+    damage = wizard_level * 2
+    new_mana = start_mana - 10
+    return damage, new_mana # return two values
+
+#We can store the multiple values returned throught the fxn
+
+one, two = cast_iceblast(5, 100)
+print(f"Damage: {one}, Remaining Mana: {two}")
+# Damage: 10, Remaining Mana: 90
+
+#Defining the default values to the fxn
+
+def get_greeting(email, name="there"):
+    print("Hello", name, "welcome! You've registered your email:", email)
+
+print("="*20)
